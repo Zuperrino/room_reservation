398eb4d7823ea38be0c875a327bb07cf5dcd46d5
Vasiliy Fursov <deefreez@yandex.ru>
2025-09-07 13:41:56 +0300

    fix

--- alembic/env.py
+++ alembic/env.py
@@ -1,15 +1,23 @@
 import asyncio
+import os
 from logging.config import fileConfig
 
+from dotenv import load_dotenv
 from sqlalchemy import pool
 from sqlalchemy.engine import Connection
 from sqlalchemy.ext.asyncio import async_engine_from_config
 
 from alembic import context
 
+from app.core.db import Base
+
+load_dotenv('.env')
+
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+
+config.set_main_option('sqlalchemy.url', os.environ['DATABASE_URL'])
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
@@ -20,7 +28,8 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = Base.metadata
+print(target_metadata.tables)
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
