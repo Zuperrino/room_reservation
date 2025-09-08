b9b6354f61075f01a2c48c131cbce93b254f47fa
Vasiliy Fursov <deefreez@yandex.ru>
2025-09-08 11:44:20 +0300

    prefix

---
+++ app/schemas/meeting_room.py
@@ -0,0 +1,7 @@
+from typing import Optional
+from pydantic import BaseModel, Field
+
+
+class MeetingRoomCreate(BaseModel):
+    name: str = Field(min_length=1, max_length=100)
+    description: Optional[str]
